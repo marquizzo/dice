c3ce516e2015001eb3366cd343d13ba2942b1d86
Marquizzo <marco@dyadstudios.com>
2022-02-03 22:53:40 -0800

    Walls, part 1.

--- src/webgl/Walls.ts
+++ src/webgl/Walls.ts
@@ -1,11 +1,26 @@
 import * as THREE from "three";
+import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils"; 
 import * as CANNON from "cannon-es";
 
 export default class Walls {
-	public mesh: THREE.Mesh;
-	public body: CANNON.Body;
+	private mat: THREE.Material;
+	private mesh: THREE.Mesh;
+	private body: CANNON.Body;
 
 	constructor() {
+		// const width;
+		this.mat = new THREE.MeshNormalMaterial();
+		const geomLeft = new THREE.PlaneGeometry(10, 10);
+		geomLeft.rotateY(Math.PI / 2);
+		geomLeft.translate(-6, 0, 0);
+		const geomRight = new THREE.PlaneGeometry(10, 10);
+		geomRight.rotateY(-Math.PI / 2);
+		geomRight.translate(6, 0, 0);
+
+		const geomAll = mergeBufferGeometries([geomLeft, geomRight]);
+		this.mesh = new THREE.Mesh(geomAll, this.mat);
+
+
 		this.body = new CANNON.Body({
 			type: CANNON.Body.STATIC,
 			shape: new CANNON.Plane()
@@ -15,6 +30,7 @@ export default class Walls {
 	}
 
 	public addToView(scene: THREE.Scene, world: CANNON.World) {
+		scene.add(this.mesh);
 		world.addBody(this.body);
 	}
 }
